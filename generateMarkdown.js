68417b0948b9ff5db2cd7695a842278bbf2b8cac
Rheanne Reyes <rheannemr@gmail.com>
2021-01-21 17:05:58 -0800

    Add questions and license

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,45 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-pink.svg)](https://opensource.org/licenses/MIT)';
+    case "Apache 2.0":
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case "Eclipse":
+      return '[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return '[MIT](https://opensource.org/licenses/MIT)';
+    case "Apache 2.0":
+      return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
+    case "Eclipse":
+      return '[Eclipse Public License 1.0](https://opensource.org/licenses/EPL-1.0)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return renderLicenseLink(license) + renderLicenseBadge(license);
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+function generateMarkdown({title, license, description}) {
+  return `# ${title} \n
+  license: ${renderLicenseSection(license)} \n
+  description: ${description} \n
+  
 `;
 }
 
--- index.js
+++ index.js
@@ -1,27 +1,82 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const {writeFile} = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title for this project?',
+        message: 'Project title:',
         validate: (input) => !!input.length
     },
     {
-        type: '',
-        name: '',
-        message: '',
-        validate:
+        type: 'input',
+        name: 'description',
+        message: 'Project description:'
     },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Link to deployed application:'
+    },
+    {
+        type: 'input',
+        name: 'prerequisites',
+        message: 'Prerequisites:'
+    },
+    {
+        type: 'input',
+        name: 'credit',
+        message: 'Contributors:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How to install:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How to use application:'
+    },
+    {
+        type: 'input',
+        name: 'credit',
+        message: 'Contributors:'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Github username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Email address:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'License used for project:',
+        choices: ['MIT', 'Apache 2.0', 'Eclipse']
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    writeFile(fileName, data, (err) => console.log('error in write file:', err));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const answers = await inquirer.prompt(questions);
+    console.log('answers are: ', answers);
+    const markDown = generateMarkdown(answers);
+    console.log('generated markdown: ', markDown);
+    writeToFile(answers.title + '_README.md', markDown);
+}
 
 // Function call to initialize app
 init();
